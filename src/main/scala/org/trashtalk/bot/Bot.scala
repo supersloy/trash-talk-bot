b474032d4199567202a45ddb40145ac3cc07a954
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2022-03-14 03:25:59 +0500

    Added new message types for the bot

--- src/main/scala/org/trashtalk/bot/Bot.scala
+++ src/main/scala/org/trashtalk/bot/Bot.scala
@@ -1,87 +1,78 @@
 package org.trashtalk.bot
 
 import cats.effect._
-import cats.syntax.functor._
-import com.bot4s.telegram.models._
-import com.bot4s.telegram.cats.Polling
-import com.bot4s.telegram.cats.TelegramBot
+import cats.syntax.all._
+import com.bot4s.telegram.api.declarative.{
+  Action,
+  CommandFilterMagnet,
+  Commands,
+}
+import com.bot4s.telegram.cats.{Polling, TelegramBot}
 import com.bot4s.telegram.methods._
-import doobie.util.transactor.Transactor
-import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
-import sttp.client3.SttpBackend
+import com.bot4s.telegram.models._
 import doobie.implicits._
-import cats.syntax.flatMap._
-import com.bot4s.telegram.models.InputFile.FileId
+import doobie.util.transactor.Transactor
 import org.trashtalk.bot.Schemas.MsgType
-import SQLCommands.DBMessage
-import com.bot4s.telegram.api.declarative.{Action, CommandFilterMagnet, Commands}
-import com.typesafe.scalalogging.Logger
+import sttp.client3.SttpBackend
+import sttp.client3.asynchttpclient.cats.AsyncHttpClientCatsBackend
 
-
-
-
-class Bot[F[_] : Async](token: String, backend: SttpBackend[F, Any], xa: Transactor[F])
-  extends TelegramBot[F](token, backend)
+class Bot[F[_]: Async](
+    token: String,
+    backend: SttpBackend[F, Any],
+    xa: Transactor[F],
+    telegramApi: String = "api.telegram.org",
+) extends TelegramBot[F](token, backend, telegramApi)
     with Polling[F]
-    with Commands[F]
-    {
+    with Commands[F] {
 
+  onCommand("start") { implicit msg =>
+    request(SendMessage(msg.chat.chatId, "go fuck yourself")).void
+  }
 
-  override val logger: Logger = ???
-
-
-  override def onCommand(filter: CommandFilterMagnet)(action: Action[F, Message]): Unit = super.onCommand(filter)(action)
-
-  def generateMessage(chat_id : Long): F[Unit] = {
+  def generateMessage(chat_id: Long): F[Unit] =
     SQLCommands
       .getRandomMessage(chat_id)
       .transact(xa)
-      .flatMap(list => {
+      .flatMap { list =>
         list.headOption match {
           case Some(msg) =>
             val content = msg.msgType match {
-              case MsgType.TEXT => SendMessage(chat_id, msg.content)
-              case MsgType.IMAGE => SendPhoto(chat_id, FileId(msg.content))
-              case MsgType.STICKER => SendSticker(chat_id, FileId(msg.content))
-              case MsgType.GIF => SendVideo(chat_id, FileId(msg.content))
+              case MsgType.TEXT  => SendMessage(chat_id, msg.content)
+              case MsgType.IMAGE => SendPhoto(chat_id, InputFile(msg.content))
+              case MsgType.STICKER =>
+                SendSticker(chat_id, InputFile(msg.content))
+              case MsgType.VIDEO => SendVideo(chat_id, InputFile(msg.content))
+              case MsgType.DOC => SendDocument(chat_id, InputFile(msg.content))
             }
             request(content).void
           case None =>
-            cats.Applicative[F].unit
+            ().pure[F]
         }
-      })
-  }
+      }
 
-//  def generateMessageGivenType(t: MsgType): F[Unit]
+  //  def generateMessageGivenType(t: MsgType): F[Unit]
 
-  override def receiveMessage(msg: Message): F[Unit] = {
+  override def receiveMessage(msg: Message): F[Unit] =
     for {
+      _ <- Async[F].delay(logger.info(msg.toString))
       _ <- SQLCommands.insertMessage(msg).transact(xa)
       _ <- generateMessage(msg.chat.id)
     } yield ()
 
-  }
 }
 
 object Bot extends IOApp {
-  import doobie._
-  import doobie.Transactor
-  import doobie.util.transactor._
-  import cats._
-  import cats.data._
-  import cats.effect._
-  import cats.implicits._
-  import scala.concurrent._
 
+  import cats.effect._
+  import doobie.{Transactor, _}
+  import doobie.util.transactor._
 
   val botClientBackend: IO[SttpBackend[IO, Any]] =
     AsyncHttpClientCatsBackend[IO]()
 
   override def run(args: List[String]): IO[ExitCode] =
     for {
-      token
-
-        <- IO.fromOption(sys.env.get("TRASHTALK_TOKEN"))(
+      token <- IO.fromOption(sys.env.get("TRASHTALK_TOKEN"))(
         new Exception(
           "Telegram Bot API token is not set. " +
             "Please set the \"TRASHTALK_TOKEN\" environment variable" +
@@ -89,26 +80,25 @@ object Bot extends IOApp {
         )
       )
       backend <- botClientBackend
-//      dummy = Transactor(
-//        (),
-//        (_: Unit) => Resource[IO, java.sql.Connection].pure(null),
-//        KleisliInterpreter[IO](
-//          WeakAsyncConnectionIO[IO].liftExecutionContext()
-//        ).ConnectionInterpreter,
-//        Strategy.void
-//      )
-      xa <- IO.delay(Transactor.fromDriverManager[IO](
-        driver = "org.postgresql.Driver",
-        url = "jdbc:postgresql://localhost:5432/postgres",
-        user = "postgres",
-        pass = "changeme"
-      ))
+      dummy = Transactor[IO, Unit](
+        (),
+        _ => Resource.pure(null),
+        KleisliInterpreter[IO].ConnectionInterpreter,
+        Strategy.void,
+      )
+      xa <- IO.delay(
+        Transactor.fromDriverManager[IO](
+          driver = "org.postgresql.Driver",
+          url = "jdbc:postgresql://localhost:5432/postgres",
+          user = "postgres",
+          pass = "changeme",
+        )
+      )
       bot <- IO.pure(new Bot(token, backend, xa))
       botResource = Resource.make(bot.run().start)(_ =>
-        IO.blocking(bot.shutdown()),
+        IO.blocking(bot.shutdown())
       )
       _ <- botResource.use(_ => IO.never)
-    }
-    yield ExitCode.Success
+    } yield ExitCode.Success
 
 }
--- src/main/scala/org/trashtalk/bot/DB.scala
+++ src/main/scala/org/trashtalk/bot/DB.scala
@@ -1,18 +1,12 @@
 package org.trashtalk.bot
 
-import doobie._
-import doobie.implicits._
-import doobie.util.ExecutionContexts
-import cats._
-import cats.data._
-import cats.effect._
 import cats.syntax.all._
 import com.bot4s.telegram.models.Message
-import org.trashtalk.bot.Schemas.MsgType.STICKER
-import shapeless.{::, HNil}
-import Fragments.values
-import doobie.postgres._
+import doobie._
+import doobie.implicits._
 import doobie.postgres.implicits._
+
+import scala.collection.immutable.SortedMap
 
 object Schemas {
 
@@ -20,26 +14,30 @@ object Schemas {
 
   object MsgType {
     case object TEXT extends MsgType
-    case object IMAGE extends MsgType
-    case object STICKER extends MsgType
-    case object GIF extends MsgType
 
-    val msg_types = List(TEXT, IMAGE, STICKER, GIF)
+    case object IMAGE extends MsgType
+
+    case object STICKER extends MsgType
+
+    case object VIDEO extends MsgType
+
+    case object DOC extends MsgType
+
+    private val mapping = SortedMap(
+      "TEXT"    -> TEXT,
+      "IMAGE"   -> IMAGE,
+      "STICKER" -> STICKER,
+      "VIDEO"   -> VIDEO,
+      "DOC"     -> DOC,
+    )
+
+    val msg_types: List[MsgType] = mapping.values.toList
     def toEnum(e: MsgType): String = e.toString
 
-    def fromEnum(s: String): Option[MsgType] = s match {
-      case "TEXT" => Some(TEXT)
-      case "IMAGE" => Some(IMAGE)
-      case "STICKER" => Some(STICKER)
-      case "GIF" => Some(GIF)
-
-      case _ => None
-    }
-
+    def fromEnum(s: String): Option[MsgType] = mapping.get(s)
     implicit val msgTypeMeta: Meta[MsgType] =
-      pgEnumStringOpt("myenum", MsgType.fromEnum, MsgType.toEnum)
+      pgEnumStringOpt("msg_type", MsgType.fromEnum, MsgType.toEnum)
   }
-
 
   import MsgType._
 
@@ -52,7 +50,7 @@ object Schemas {
        );
        """
 
-  val message =
+  val message: Fragment =
     fr"""CREATE TYPE msg_type AS ENUM""" ++
       Fragment.const(msg_types.map(t => s"'$t'").mkString("(", ", ", ")")) ++
       fr""";
@@ -66,22 +64,22 @@ object Schemas {
           |);
       """.stripMargin
 
-  val admin_relation =
+  val admin_relation: Fragment =
     sql"""
-        |CREATE TABLE "admin_relation" (
-        |"chat_id" bigint NOT NULL,
-        |"admin_id" bigint NOT NULL,
-        | FOREIGN KEY (chat_id) REFERENCES "chat"("chat_id"),
-        | FOREIGN KEY (admin_id) REFERENCES "admin"("user_id")
-        |);
+         |CREATE TABLE "admin_relation" (
+         |"chat_id" bigint NOT NULL,
+         |"admin_id" bigint NOT NULL,
+         | FOREIGN KEY (chat_id) REFERENCES "chat"("chat_id"),
+         | FOREIGN KEY (admin_id) REFERENCES "admin"("user_id")
+         |);
          """.stripMargin
 
   val admin: Fragment =
     sql"""
-      |CREATE TABLE "admin" (
-      |  "user_id" bigint NOT NULL,
-      |  PRIMARY KEY ("user_id")
-      |);
+         |CREATE TABLE "admin" (
+         |  "user_id" bigint NOT NULL,
+         |  PRIMARY KEY ("user_id")
+         |);
          """.stripMargin
 }
 
@@ -89,19 +87,29 @@ object SQLCommands {
 
   import org.trashtalk.bot.Schemas._
 
-  case class DBMessage(chatId: Long, messageId: Long, msgType: MsgType, content: String)
+  case class DBMessage(
+      chatId: Long,
+      messageId: Long,
+      msgType: MsgType,
+      content: String,
+  )
 
   object DBMessage {
-    def from(msg: Message): Option[DBMessage] = {
-      ((msg.text, msg.sticker) match {
-        case (Some(_), Some(_)) => None
-        case (Some(text), None) => Some((MsgType.TEXT, text))
-        case (None, Some(sticker)) => Some((MsgType.STICKER, sticker.fileId))
-        case (None, None) => None
-      }).map {
-        case (msgType, content) => DBMessage(msg.chat.id, msg.messageId, msgType, content)
+    def from(msg: Message): Option[DBMessage] =
+      ((msg.text, msg.sticker, msg.video, msg.photo, msg.document) match {
+        case (Some(text), None, None, None, None) => Some((MsgType.TEXT, text))
+        case (None, Some(sticker), None, None, None) =>
+          Some((MsgType.STICKER, sticker.fileId))
+        case (None, None, Some(video), None, None) =>
+          Some((MsgType.VIDEO, video.fileId))
+        case (None, None, None, Some(photo +: _), None) =>
+          Some((MsgType.IMAGE, photo.fileId))
+        case (None, None, None, None, Some(document)) =>
+          Some((MsgType.DOC, document.fileId))
+        case _ => None
+      }).map { case (msgType, content) =>
+        DBMessage(msg.chat.id, msg.messageId, msgType, content)
       }
-    }
   }
 
   val reCreateSchema: ConnectionIO[Int] =
@@ -112,11 +120,11 @@ object SQLCommands {
 
   val createTables: ConnectionIO[Int] = (
     chat ++
-    message
-    ++ admin ++ admin_relation
-    ).update.run
+      message
+      ++ admin ++ admin_relation
+  ).update.run
 
-  def getRandomMessage(source_id : Long) =
+  def getRandomMessage(source_id: Long): ConnectionIO[List[DBMessage]] =
     sql"""
       SELECT chat_id, message_id, type, content FROM message
       WHERE chat_id = $source_id
@@ -124,37 +132,37 @@ object SQLCommands {
       LIMIT 1
        """.query[DBMessage].to[List]
 
-  // https://tpolecat.github.io/doobie/docs/07-Updating.html
-  def insertMessage(msg: Message): ConnectionIO[Unit] = {
-    DBMessage.from(msg).map(m => {
-      sql"""
+  def insertMessage(msg: Message): ConnectionIO[Unit] =
+    DBMessage
+      .from(msg)
+      .map { m =>
+        sql"""
            INSERT INTO chat (chat_id) values (${m.chatId}) ON CONFLICT DO NOTHING;
-         """.update.run*>
-      sql"""insert into message (message_id, chat_id, content, type) values (${m.messageId}, ${m.chatId}, ${m.content}, ${m.msgType.toString}::msg_type);"""
-        .update.run.void
-    }
-    ).getOrElse(
-      cats.Applicative[ConnectionIO].unit
-    )
+         """.update.run *>
+          sql"""insert into message (message_id, chat_id, content, type) values (${m.messageId}, ${m.chatId}, ${m.content}, ${m.msgType.toString}::msg_type);""".update.run.void
+      }
+      .getOrElse(
+        cats.Applicative[ConnectionIO].unit
+      )
 
-  }
 }
 
 object DBConnect {
   def main(args: Array[String]): Unit = {
+    import SQLCommands._
     import cats.effect.IO
     import cats.effect.unsafe.implicits.global
-    import SQLCommands._
 
     val transactor = Transactor.fromDriverManager[IO](
       driver = "org.postgresql.Driver",
       url = "jdbc:postgresql://localhost:5432/postgres",
       user = "postgres",
-      pass = "changeme"
+      pass = "changeme",
     )
 
     (reCreateSchema *> createTables)
-      .transact(transactor).unsafeRunSync()
+      .transact(transactor)
+      .unsafeRunSync()
   }
 
 }
